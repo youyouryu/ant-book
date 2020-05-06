bd3891779f1d8d0fe4797c361e84001b752dc3dc
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-05-06 16:43:39 +0900

    fix p052 to memorize

--- ch02/p052/main.go
+++ ch02/p052/main.go
@@ -29,21 +29,36 @@ func solve(stdin io.Reader, stdout io.Writer) {
 		weights = append(weights, w)
 		values = append(values, v)
 	}
-
-	a := search(n, wMax, weights, values, 0)
+	memo := map[param]int{}
+	a := search(n, wMax, weights, values, 0, &memo)
 	fmt.Fprintln(stdout, a)
 }
 
-func search(n, w int, weights, values []int, idx int) int {
+type param struct {
+	w   int
+	idx int
+}
+
+func search(n, w int, weights, values []int, idx int, memo *map[param]int) int {
 	if idx == n {
 		return 0
 	}
-	a0 := search(n, w, weights, values, idx+1)
+	a0 := remindAndMemorize(n, w, weights, values, idx+1, memo)
 	if w >= weights[idx] {
-		a1 := values[idx] + search(n, w-weights[idx], weights, values, idx+1)
+		a1 := values[idx] + remindAndMemorize(n, w-weights[idx], weights, values, idx+1, memo)
 		if a0 < a1 {
 			return a1
 		}
 	}
 	return a0
+}
+
+func remindAndMemorize(n, w int, weights, values []int, idx int, memo *map[param]int) (a int) {
+	if v, ok := (*memo)[param{w, idx}]; ok {
+		a = v
+	} else {
+		a = search(n, w, weights, values, idx, memo)
+		(*memo)[param{w, idx}] = a
+	}
+	return a
 }
