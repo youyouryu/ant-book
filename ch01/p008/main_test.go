756bb4d6379ded024b48c603f04597bd734146f0
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-05-21 12:06:13 +0900

    fix test case of ch01

--- ch01/p008/main_test.go
+++ ch01/p008/main_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestSolve1(t *testing.T) {
 	if err := common.Check(solve, "test/q1.txt", "test/a1.txt"); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 }
 
 func TestSolve2(t *testing.T) {
 	if err := common.Check(solve, "test/q2.txt", "test/a2.txt"); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 }
--- ch01/p023/main_test.go
+++ ch01/p023/main_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestSolve1(t *testing.T) {
 	if err := common.Check(solve, "test/q1.txt", "test/a1.txt"); err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 }
