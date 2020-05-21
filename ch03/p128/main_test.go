1cb09ed64e2e875d6d89cf18c0dbdf9a614ed429
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-05-21 13:51:08 +0900

    fix p128

--- ch03/p128/main_test.go
+++ ch03/p128/main_test.go
@@ -7,17 +7,25 @@ import (
 )
 
 func TestSolve1(t *testing.T) {
-	common.Check(solve, "test/1.in", "test/1.out")
+	if err := common.Check(solve, "test/1.in", "test/1.out"); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestSolve2(t *testing.T) {
-	common.Check(solve, "test/2.in", "test/2.out")
+	if err := common.Check(solve, "test/2.in", "test/2.out"); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestSolve3(t *testing.T) {
-	common.Check(solve, "test/3.in", "test/3.out")
+	if err := common.Check(solve, "test/3.in", "test/3.out"); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestSolve4(t *testing.T) {
-	common.Check(solve, "test/4.in", "test/4.out")
+	if err := common.Check(solve, "test/4.in", "test/4.out"); err != nil {
+		t.Error(err)
+	}
 }
