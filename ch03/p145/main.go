4fa4e73eacf6b9176e06fa00cc9cc14047296dce
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-06-05 14:02:32 +0900

    temporal commit

--- ch03/p145/main.go
+++ ch03/p145/main.go
@@ -28,14 +28,15 @@ func solve(stdin io.Reader, stdout io.Writer) {
 	t, _ := strconv.ParseFloat(sc.Text(), 64)
 	ans := calcHeights(n, h, r, t)
 	for _, v := range ans {
-		fmt.Fprintln(stdout, v)
+		fmt.Fprintf(stdout, "%.2f\n", v)
 	}
 }
 
 func calcHeights(n int, h, r, t float64) (y []float64) {
 	y = []float64{}
 	for i := 0; i < n; i++ {
-		yi := calcHeight(i, h, r, t)
+		//yi := calcHeight(h+2*r*float64(i), t-float64(i))
+		yi := calcHeight(h, t-float64(i))
 		y = append(y, yi)
 	}
 	sort.Slice(y, func(i, j int) bool { return y[i] < y[j] })
@@ -45,24 +46,21 @@ func calcHeights(n int, h, r, t float64) (y []float64) {
 	return y
 }
 
-func calcHeight(i int, h, r, t float64) (y float64) {
-	fmt.Println(i, h, r, t)
-	h += 2.0 * r * float64(i)
-	if t < float64(i) {
+func calcHeight(h, t float64) (y float64) {
+	if t < 0 {
 		return h
 	}
-	t -= float64(i)
-	pt := calcPeriodicTime(h)
-	for t > pt {
-		t -= pt
+	ft := calcFallTime(h)
+	k := int(t / ft)
+	var d float64
+	if k%2 == 0 {
+		d = t - float64(k)*ft
+	} else {
+		d = float64(k)*ft + ft - t
 	}
-	fmt.Println(t, pt)
-	if t <= pt/2.0 {
-		return h - g*t*t/2.0
-	}
-	return h - g*(t-pt)*(t-pt)/2.0
+	return h - g*d*d*0.5
 }
 
-func calcPeriodicTime(h float64) (periodicTime float64) {
-	return math.Sqrt(2.0*h/g) * 2.0
+func calcFallTime(h float64) (periodicTime float64) {
+	return math.Sqrt(2.0 * h / g)
 }
