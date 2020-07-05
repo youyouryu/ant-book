956cceb17b7ecae4c3a03cc877382f3297767485
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-07-05 17:06:36 +0900

    add p162

---
+++ ch03/p162/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/youyouryu/AtCoder/lib"
+)
+
+func main() {
+	io := lib.NewIo(os.Stdin, os.Stdout)
+	defer io.Flush()
+	n := io.NextInt()
+	a := io.NextInts(n)
+	ans := solve(a)
+	io.Println(ans)
+}
+
+func solve(a []int) (ans int) {
+	fmt.Println(a)
+	bit := lib.NewBinaryIndexedTree(len(a))
+	for i := range a {
+		ans += i - bit.Sum(a[i])
+		bit.Add(a[i], 1)
+
+		fmt.Println(bit)
+	}
+	return ans
+}
